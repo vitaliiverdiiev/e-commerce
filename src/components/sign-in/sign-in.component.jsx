96e0c12fc2b6390c9ecc324f32ade7b4c4f95ee7
Vitalii Verdiiev <vitaliiverdiiev@gmail.com>
2021-07-16 19:21:59 +0300

    Added SignIn, FormInput and Button components

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2 className="title">I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="Email"
+            required
+          />
+
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="Password"
+            required
+          />
+
+          <Button type="submit" children="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
